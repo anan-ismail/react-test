283033e6709101013d2594382b6ec5631ecd8e8f
Anan Ismail <75697420+anan-ismail@users.noreply.github.com>
2021-04-17 19:17:20 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          pls save me
+          <a
+          className= "App-link"
+          href= "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target= "_blank"
+          rel= "noopener noreferrer">
+            click here to get your dream gf or bf or apache attack helicopter idc
+          </a>
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          help save me
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
